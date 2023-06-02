a928f40738c82a3e0cb7cd2b6a9814eaa8b7aab0
abhishek <abhishek251119@gmail.com>
2023-06-02 09:05:55 +0530

    function Component && class Component && State && Props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import React,{ useState } from 'react';
+import Student from './Student';
 
 function App() {
+  const[data,setData]=useState(0)
+  const UpdateData = ()=>{
+    setData(data+1)
+    // alert(data)
+    // console.log('////////')
+  }
+  const [name,setName]=useState('abhishek')
+  const UpdateName = function(){
+    setName('Abhishek Tiwari')
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>{data} </h1>
+        <button onClick={UpdateData}>Clike me</button>
+        <Student name={name} />
+        {/* <Student name='abhishek' address={{mobile:'4444',location:'katni'}}/> */}
+        <button onClick={UpdateName}>update name</button>
     </div>
   );
 }
 
+
+
 export default App;
+
+
