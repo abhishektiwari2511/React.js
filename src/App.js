4ad8d3514e14cf790e9622492da996b3ce257181
abhishek <abhishek251119@gmail.com>
2023-06-02 10:13:22 +0530

    get input box value

--- src/App.js
+++ src/App.js
@@ -1,27 +1,24 @@
 // import logo from './logo.svg';
 import './App.css';
 import React,{ useState } from 'react';
-import Student from './Student';
+
 
 function App() {
-  const[data,setData]=useState(0)
-  const UpdateData = ()=>{
-    setData(data+1)
-    // alert(data)
-    // console.log('////////')
-  }
-  const [name,setName]=useState('abhishek')
-  const UpdateName = function(){
-    setName('Abhishek Tiwari')
-  }
-  
+  const [text,setText]=useState(null)
+  const [valu,setValue]=useState(false)
+ const abhi= (event)=>{
+  // console.log(event.target.value)
+setText(event.target.value)
+setValue(false)
+ }
+
   return (
     <div className="App">
-        <h1>{data} </h1>
-        <button onClick={UpdateData}>Clike me</button>
-        <Student name={name} />
-        {/* <Student name='abhishek' address={{mobile:'4444',location:'katni'}}/> */}
-        <button onClick={UpdateName}>update name</button>
+        {
+          valu===true?<h1>{text}</h1>:null
+        }
+        <input type='text' onChange={abhi}></input>
+        <button onClick={()=>setValue(true)}>click</button>
     </div>
   );
 }
