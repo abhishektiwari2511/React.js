9293bb0caef29c24d22d5823203eead9fdcc5320
abhishek <abhishek251119@gmail.com>
2023-06-04 09:02:38 +0530

    function as a props for 2 number sum

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 // import logo from './logo.svg';
 import "./App.css";
-import User from "./User";
+import {User,Users} from "./User";
+import {useState} from "react"
 function App() {
+  const [user1,setUser1]=useState(0)
+  const [user2,setUser2]=useState(0)
+  const [user,setUser]=useState(0)
   function getData(){
     return (
     alert("you are excess user page help of props because i send a function as a props")
     )
   }
+  function Sum(){
+    let a=user1
+    let b=user2
+    let c= (a+b)
+    console.log(user1.type,user2.type,c)
+     setUser(c)
+  }
  return (
     <div className="App">
+      <h1>sum 2 no</h1>
+      {/* <input type="number" placeholder="enter no 1" onChange={(e)=>{setUser1(e.target.valueAsNumber )}}/><br/>
+      <input type="number" placeholder="enter no 2" onChange={(e)=>{setUser2(e.target.valueAsNumber)}}/><br/>
       
-     <User data={getData}/>
+      {!(user===null)?<span>{user}</span>:'please enter value'} */}
+     <User  sum={Sum} user={user} setUser1={setUser1} setUser2={setUser2}/>
+     <Users data={getData}/>
     
     </div>
   );
--- src/User.js
+++ src/User.js
@@ -1,8 +1,24 @@
-export default function User(props){
+
+function User(props){
+
     return(
         <div>
-           <h1>hello word</h1>
-           <button onClick={props.data}>click</button>
+             
+      <input type="number" placeholder="enter no 1" onChange={(e)=>{props.setUser1(e.target.valueAsNumber )}}/><br/>
+      <input type="number" placeholder="enter no 2" onChange={(e)=>{props.setUser2(e.target.valueAsNumber)}}/><br/>
+      
+      {!(props.user===0)?<span>{props.user}</span>:<label>please enter value</label>}<br/>
+            <button onClick={props.sum}>click</button>
         </div>
     )
+
 }
+ function Users(props){
+    return(
+                <div>
+                   <h1>hello word</h1>
+                   <button onClick={props.data}>click</button>
+                </div>
+            )
+        }
+        export{User,Users}
