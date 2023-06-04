c86d314f233c695bfb26fb5a6ce2c088d61cf052
abhishek <abhishek251119@gmail.com>
2023-06-05 01:13:50 +0530

    Should component Update Life Cycle mathod

--- README.md
+++ README.md
@@ -68,3 +68,6 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+
+<!-- git push --set-upstream https://github.com/abhishektiwari2511/React.js.git ComponentDidUpdate-Life-Cycle-Mathod -->
--- src/App.js
+++ src/App.js
@@ -5,27 +5,28 @@ import React from "react"
 class App extends React.Component{
   constructor(){
     super()
-    console.log("constructor")
     this.state={
       count:0
     }
+    console.log("constructor")
+  
   }
-  componentDidUpdate(preProps,preState,sanpshort){
-    console.log('componentDidUpdate',preState.count,this.state.count)
-  //  if(this.state.count===preState.count){
-  //   alert('you data is alredy same')
-  //  }
-   if(this.state.count<10){
-    this.setState({count:this.state.count+1})
-   }
-   
+  shouldComponentUpdate(){
+    console.log('ShouldComponentUpdate',this.state.count)
+  
+ if(this.state.count>5 && this.state.count<10 ){
+  return true
+ }
+ else{
+  return false
+ }
   }
   render(){
     console.log("render",this.state.count)
     return(
       <div>
-        <h1>Component Did Update {this.state.count}</h1>
-        <button onClick={()=>this.setState({count:1})}>click</button>
+        <h1> Should Component Update {this.state.count} </h1>
+        <button onClick={()=>this.setState({count:this.state.count+1})}>click</button>
      
       </div>
     )
