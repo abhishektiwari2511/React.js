bf7499c974177f646d8cbc9ad04d30d52bd0429d
abhishek <abhishek251119@gmail.com>
2023-06-04 21:22:26 +0530

    componentDidMount Life Cycle Method

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 // import logo from './logo.svg';
 import "./App.css";
 import React from "react"
-import User from "./User";
-function App(){
-  const [data,setData]=React.useState('abhi')
-  return(
-    <div>
-      <User data={data}/>
-      <button onClick={()=>setData('abhishek')}>Name update</button><br/>
-    </div>
-  )
+
+class App extends React.Component{
+  constructor(){
+    super()
+    console.log("constructor")
+    this.state={
+      name:"abhishek"
+    }
+  }
+  componentDidMount(){
+    console.log('componentDidMount',this.state.name)
+    // this.setState({name:"ram"})
+  }
+  render(){
+    console.log("render",this.state.name)
+    return(
+      <div>
+        <h1>hello {this.state.name}</h1>
+      <button onClick={()=>this.setState({name:"ram"})}> click</button>
+      </div>
+    )
+  }
+
 }
 
 export default App;
