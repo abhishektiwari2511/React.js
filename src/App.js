5c471a48b33623b6dc939c2656f50b08b58cd0e4
abhishek <abhishek251119@gmail.com>
2023-06-06 00:57:36 +0530

    bootstrap with react

--- src/App.js
+++ src/App.js
@@ -1,20 +1,45 @@
 // import logo from './logo.svg';
 import "./App.css";
-import User from "./User";
+import {Table} from "react-bootstrap"
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-import React,{useState} from "react"
+
+// import React,{useState} from "react"
  function App(){
-  const [count,setCount]=useState(0)
-  const [data,setData]=useState(100)
+  // const [count,setCount]=useState(0)
+  // const [data,setData]=useState(100)
+  const data=[
+    {name:"abhishek",email:"abhishek@gmail",contact: 100},
+    {name:"abhi",email:'abhi@gmail',contact: 100},
+    {name:"ram",email:'abhishek@gmail',contact: 300},     
+  ]
+  // data.map((item)=>console.log(item))
 
   return(
     <div className="App">
-     
-      <User count={count} data={data} />
-      <button onClick={()=>setCount(count+1)}>Update Count</button><br/>
-      <button onClick={()=>setData(data-1)}>Update Data</button>
-     
-    </div>
+     <h1>Data list</h1>
+
+     <Table striped variant="dark" >
+      <tbody>
+        <tr>
+          <td>Name</td>
+          <td>Email</td>
+          <td>Contact</td>
+        </tr>
+     {
+      data.map((item,i)=>
+      // <h1>name is = {item.name} ,email = {item.email} , contact ={item.contact}</h1>
+      item.email==="abhishek@gmail" || item.contact===100?  
+      <tr key={i}>
+          <td>{item.name}</td>
+          <td>{item.email}</td>
+          <td>{item.contact}</td>
+        </tr>:null
+      )
+    }
+    </tbody>
+    </Table>
+     </div>
   )
  }
 
