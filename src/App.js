4d32f29d1fd2fd63c67131c3edba8c26e53496fa
abhishek <abhishek251119@gmail.com>
2023-06-07 20:11:14 +0530

    useRef for function component

--- src/App.js
+++ src/App.js
@@ -1,63 +1,23 @@
 // import logo from './logo.svg';
 import "./App.css";
-import React,{createRef,Component} from "react"
+import React,{useRef} from "react"
+function App(){
+    let ram= useRef(null);
+    console.warn(ram)
+    function vishnu(){
+        console.log(ram.current.value)
+        ram.current.focus()
+        // ram.current.style.display='none'
+        ram.current.style.color='pink'
+        ram.current.style.backgroundColor='grey'
 
-class App extends Component {
-  constructor(){
-    super()
-    this.ram= createRef()
-  }
-  componentDidMount(){
-    // console.log(this.ram.current.value='1000')
-    console.log(this.ram.current.value)
-  }
-  ramAaaye(){
-    console.log(this.ram.current.value)
-    this.ram.current.style.color='white'
-    this.ram.current.style.backgroundColor='gray'
-    // this.ram.current.value=this.ram.current.value*5
-  }
-
-  render(){
-    return(
-      <div className='App'>
-        <h1>hello world</h1>
-        <input type="text" ref={this.ram}/>
-        <button onClick={()=>this.ramAaaye()}>click</button>
-      </div>
+    }
+    return (
+        <div className="App">
+            <h1>hello world </h1>
+            <input type="text" ref={ram} />
+            <button onClick={vishnu}>click me</button>
+        </div>
     )
-  }
 }
-export default App
-
-// import "bootstrap/dist/css/bootstrap.min.css";
-// import React,{createRef} from "react"
-
-// class App extends React.Component{
-// constructor(){
-//   super()
-//   this.inputRet=createRef()
-// }
-// componentDidMount(){
-//   console.log(this.inputRet)
-// }
-//   getVal(){
-//     console.log(this.inputRet.current.value)
-//     this.inputRet.current.style.color='red'
-//     this.inputRet.current.style.backgroundColor='grey'
-//     this.inputRet.current.value=this.inputRet.current.value*5
-//   }
-//   render(){
-   
-//   return(
-//     <div className="App">
-//     <h1>Ref in React </h1>
-//     <input type="text" ref={this.inputRet} / >
-//       <button onClick={()=>this.getVal()}>click</button>
-//     </div>
-//     )
-// }
-// }
-// export default App;
-
-
+ export default App
