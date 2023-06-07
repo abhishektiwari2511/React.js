6f00c9cc709440e99ff48578849ad7ba14a78950
abhishek <abhishek251119@gmail.com>
2023-06-07 19:18:55 +0530

    Ref for class component

--- src/App.js
+++ src/App.js
@@ -1,35 +1,63 @@
 // import logo from './logo.svg';
 import "./App.css";
+import React,{createRef,Component} from "react"
 
-import "bootstrap/dist/css/bootstrap.min.css";
-import React,{useMemo,useState} from "react"
+class App extends Component {
+  constructor(){
+    super()
+    this.ram= createRef()
+  }
+  componentDidMount(){
+    // console.log(this.ram.current.value='1000')
+    console.log(this.ram.current.value)
+  }
+  ramAaaye(){
+    console.log(this.ram.current.value)
+    this.ram.current.style.color='white'
+    this.ram.current.style.backgroundColor='gray'
+    // this.ram.current.value=this.ram.current.value*5
+  }
 
-
-// import {Fragment}  from "react"
-
-
-function App(){
-  const [count,setCount]=useState(0)
-  const [data,setData]=useState(10)
-  
-  const multicountMemo=useMemo(
-
-    function multicount(){
-      
-      console.log("rerandring")
-      return count*5
-    },[count])
-  return(
-    <div className="App">
-      <h1>your count is {count}</h1>
-      <h1>your data is {data}</h1>
-      <h2>{multicountMemo}</h2>
-      <button onClick={()=>setCount(count+1)}>click for count</button>
-      <button onClick={()=> setData(data+1)}>click for data</button>
-    </div>
-  )
+  render(){
+    return(
+      <div className='App'>
+        <h1>hello world</h1>
+        <input type="text" ref={this.ram}/>
+        <button onClick={()=>this.ramAaaye()}>click</button>
+      </div>
+    )
+  }
 }
+export default App
 
-export default App;
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import React,{createRef} from "react"
+
+// class App extends React.Component{
+// constructor(){
+//   super()
+//   this.inputRet=createRef()
+// }
+// componentDidMount(){
+//   console.log(this.inputRet)
+// }
+//   getVal(){
+//     console.log(this.inputRet.current.value)
+//     this.inputRet.current.style.color='red'
+//     this.inputRet.current.style.backgroundColor='grey'
+//     this.inputRet.current.value=this.inputRet.current.value*5
+//   }
+//   render(){
+   
+//   return(
+//     <div className="App">
+//     <h1>Ref in React </h1>
+//     <input type="text" ref={this.inputRet} / >
+//       <button onClick={()=>this.getVal()}>click</button>
+//     </div>
+//     )
+// }
+// }
+// export default App;
 
 
