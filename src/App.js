73166b6501c8cf92e41b14576352610d49ea92e8
abhishek <abhishek251119@gmail.com>
2023-06-08 14:45:41 +0530

    react routing

--- src/App.js
+++ src/App.js
@@ -1,30 +1,18 @@
 // import logo from './logo.svg';
 import "./App.css";
-import React, { useState } from "react";
+import{BrowserRouter,Routes,Route} from 'react-router-dom'
+import About from "./Component/About";
+import Home from "./Component/Home";
 function App() {
   return (
-    <div className="App">
-       <h1>H.O.C</h1>
-      <HocRed ram={Component}/>
-      <HocBlue ram={Component}/>
-      <h1 style={{backgroundColor:'gray',color:'white',width:'100'}}><Component/></h1>
+    <div className="App"> 
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/about" element={<About/>}/>
+    </Routes>
+    </BrowserRouter>
     </div>
-  );
-}
-function HocRed(props){
-  return  <h2 style={{backgroundColor:'red',width:'100'}}>red<props.ram /></h2>
- }
- function HocBlue (props){
-  return <h2 style={{backgroundColor:'blue',width:'100'}}>blue<props.ram /></h2>
- }
-function Component(props) {
-  const [count, setCount] = useState(0);
-  return (
-    <div>
-      <h1>your count is {count}</h1>
-      <button onClick={() => setCount(count + 1)}>click</button>
-    </div>
-  );
+  )
 }
 export default App;
-
---
+++ src/Component/About.js
@@ -0,0 +1,7 @@
+export default function About (){
+    return(
+        <div>
+            <h1>hello from About</h1>
+        </div>
+    )
+}
