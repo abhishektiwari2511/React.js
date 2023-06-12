39d579c1bc427997a1babc4004ff8de0f3a31c6c
abhishek <abhishek251119@gmail.com>
2023-06-12 12:28:09 +0530

    call post api method

--- src/App.js
+++ src/App.js
@@ -1,55 +1,52 @@
 // import logo from './logo.svg';
 import "./App.css";
-import React,{useEffect,useState} from 'react'
+import React,{useState} from 'react'
 
 
 function App() {
-  const [data,setData]=useState([])
-  useEffect(()=>{
-    fetch('http://localhost:5000/products').then((resul)=>{
-      resul.json().then((res)=>{
-        console.warn("result",res)
-        setData(res)
-      })
-    })
-  },[])
+  const [name,setName]=useState('')
+  const [email,setEmail]=useState('')
+  const [password,setPassword]=useState('')
+  function ram(){
+  let data={name,email,password}
+console.warn(data)
+fetch('http://localhost:5000/user',{
+  method:'POST',
+  headers:{
+    'Accept': 'application/json',
+    'Content-type':'application/json'
+  },
+  body:JSON.stringify(data)
+}).then((result)=>{
+  result.json().then((res)=>{
+    console.warn(res)
+  })
+})
+// fetch("http://localhost:5000/createUser", {
+//   method: "POST",
+//   headers: {
+//     'Accept': 'application/json',
+//     'Content-Type': 'application/json',
+//   },
+//   body:JSON.stringify(data)
+// }).then((resp)=>{
+//   // console.warn("resp",resp);;
+//   resp.json().then((result)=>{
+//     console.warn("result",result)
+//   })
+// })
+}
 
+return(
+  <div className="App">
+    <h1> Post Api method</h1>
+    <input type="text" name="name" value={name} onChange={(e)=>setName(e.target.value)}/><br/>
+    <input type="text" name='email' value={email} onChange={(e)=>setEmail(e.target.value)}/><br/>
+    <input type="text" name='password' value={password} onChange={(e)=>setPassword(e.target.value)}/><br/>
+    <button onClick={()=>ram()}>sumit</button>
 
-console.log(data)
-  return (
-    <div className="App">
-   <h1>Get Api Call</h1>
-   <table border="1">
-    <tbody>
-
-   <tr>
-        <td>Id</td>
-        <td>Name</td>
-        {/* <td>Link</td> */}
-        <td>Category</td>
-        <td>price</td>
-        <td>createdAt</td>
-        {/* <td>updateAt</td> */}
-      </tr>
-    {
-      data.map((item,i)=>
-      <tr key={i}>
-        <td>{item._id}</td>
-        <td>{item.name}</td>
-        {/* <td>{item.link}</td> */}
-        <td>{item.category}</td>
-        <td>{item.price}</td>
-        <td>{item.createdAt}</td>
-        {/* <td>{item.updateAt}</td> */}
-      </tr>
-      )}
-      </tbody>
-   </table>
-   
-   
-   
-    </div>
-);
+  </div>
+)
 
 }
 export default App;
