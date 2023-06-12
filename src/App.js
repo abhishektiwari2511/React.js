56b1c279beefd85271d792156a743b94157e7d33
abhishek <abhishek251119@gmail.com>
2023-06-12 15:05:52 +0530

    delete api method

--- src/App.js
+++ src/App.js
@@ -6,47 +6,63 @@ import React,{useEffect,useState} from 'react'
 function App() {
   const [data,setData]=useState([])
   useEffect(()=>{
-    fetch('http://localhost:5000/products').then((resul)=>{
-      resul.json().then((res)=>{
-        console.warn("result",res)
-        setData(res)
+getData()
+  
+  },[])
+
+function getData(){
+    fetch('http://localhost:5000/products').then((result)=>{
+      result.json().then((req)=>{
+        console.warn(req)
+        setData(req)
       })
     })
-  },[])
+}
+
+  function deleteData(id){
+   fetch(`http://localhost:5000/products/${id}`,{
+    method:"DELETE"
+   }).then((result)=>{
+    result.json().then((res)=>{
+      console.warn(res)
+    })
+   })
+   getData()
+  }
 
 
 console.log(data)
   return (
     <div className="App">
-   <h1>Get Api Call</h1>
+   <h1>Delet Api Call</h1>
    <table border="1">
-    <tbody>
-
-   <tr>
+    <tbody >
+      <tr>
         <td>Id</td>
         <td>Name</td>
-      
         <td>Email</td>
         <td>Password</td>
-      
+        <td>S.No</td>
+        <td>Component</td>
       </tr>
-    {
-      data.map((item,i)=>
-      <tr key={i}>
-        <td>{item._id}</td>
-        <td>{item.name}</td>
+      {
+        data.map((item,i) => 
+          <tr key={i}>
+            <td>{item._id}</td>
+            <td>{item.name}</td>
+            <td>{item.email}</td>
+            <td>{item.password}</td>
+            <td>{i+1}</td>
+            <button onClick={()=>deleteData(item._id)}>Delete</button>
+           
+
+          </tr>
+        )
+      }
       
-        <td>{item.email}</td>
-        <td>{item.password}</td>
-  
-      </tr>
-      )}
       </tbody>
-   </table>
-   
-   
-   
-    </div>
+      </table>   
+   </div>
 );
 
 }
