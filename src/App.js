5518336777cfd8606bf856894fec443583243fca
abhishek <abhishek251119@gmail.com>
2023-06-14 16:42:02 +0530

    useState ad a object

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 // import logo from './logo.svg';
 import "./App.css";
 import React, { useState } from "react";
-import Previous from "./previous";
+
 
 function App() {
-const [count,setCount]=useState(0)
+let [data,setData]=useState({name:'abhishek',age:25})
 
 
 
   return (
     <div className="App">
-      <h1>Your Current Count is {count}</h1>
-      {/* <Previous count={count} setCount={setCount}/>    */}
-      <Previous count={count}/>
-      <button onClick={()=>{setCount(Math.floor(Math.random()*10))}}>Click me</button>
+      <h1>name is {data.name}</h1><br/><br/>
+      <input type="text" value={data.name} onChange={(e)=>setData({age:data.age,name:e.target.value})}/><br/><br/>
+      <h1>age is {data.age}</h1><br/><br/>
+     <input type="text" value={data.age} onChange={(e)=>setData({...data,age:e.target.value})}/>
    </div>
   );
 }
