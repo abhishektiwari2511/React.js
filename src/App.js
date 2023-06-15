aa60be487540f57b7b207ce497d80f1b894973db
abhishek <abhishek251119@gmail.com>
2023-06-15 11:50:47 +0530

    useState as a object

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ let [data,setData]=useState({name:'abhishek',age:25})
   return (
     <div className="App">
       <h1>name is {data.name}</h1><br/><br/>
-      <input type="text" value={data.name} onChange={(e)=>setData({age:data.age,name:e.target.value})}/><br/><br/>
+      <input type="text" value={data['name']} onChange={(e)=>setData({age:data.age,name:e.target.value})}/><br/><br/>
       <h1>age is {data.age}</h1><br/><br/>
      <input type="text" value={data.age} onChange={(e)=>setData({...data,age:e.target.value})}/>
    </div>
